1cbc65bd753db4e749cd50047711ea5fc7d160c6
somai hamza <HAMZASOMAI@outlook.com>
2025-07-23 15:21:07 +0200

    commit: add styles global & products-list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BlankComponent } from './pages/blank/blank.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'blank', component: BlankComponent },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+    ]
+  }
+];
